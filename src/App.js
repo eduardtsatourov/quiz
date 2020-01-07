3fef6df492e663ef4117268158cea4d6aeb26aed
eduardtsatourov <54099103+eduardtsatourov@users.noreply.github.com>
2020-01-07 12:56:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
   renderQuestion = () => {
     let Question = null;
     let Answers = null;
-    // added change
+    // aaùiozjfoijqpfjqpsofjpoqsfpoqj
     if (this.state.question) {
       Question = <h3>{this.state.question.Question}</h3>;
       Answers = [];
