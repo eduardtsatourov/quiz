952b1723d136c04122932b7141c4529c07a152ca
eduard.tsatourov <tsatourov.eduard@ordina.be>
2020-01-23 15:00:24 +0100

    Updates

---
+++ precache-manifest.ccfdfb3b2245d2f0661d17c47a085ccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d8eb5cd9353c0be99844b8dee58656",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "4f586910552ecd778bac",
+    "url": "/quiz/static/css/main.042bbc64.chunk.css"
+  },
+  {
+    "revision": "60442246efa599f04580",
+    "url": "/quiz/static/js/2.c66a8e3f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/quiz/static/js/2.c66a8e3f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f586910552ecd778bac",
+    "url": "/quiz/static/js/main.ee172fa2.chunk.js"
+  },
+  {
+    "revision": "51bc986000e5cada7edb",
+    "url": "/quiz/static/js/runtime-main.34331df4.js"
+  }
+]);
